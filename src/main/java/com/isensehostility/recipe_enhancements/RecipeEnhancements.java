b91beaa43af4fadde911a4cc0274bb7fffd5a49d
ISenseHostility <yaboijarno@gmail.com>
2021-03-05 00:22:49 +0100

    why message

--- src/main/java/com/isensehostility/recipe_enhancements/RecipeEnhancements.java
+++ src/main/java/com/isensehostility/recipe_enhancements/RecipeEnhancements.java
@@ -1,10 +1,7 @@
-package com.isensehostility.re;
+package com.isensehostility.recipe_enhancements;
 
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,20 +12,6 @@ public class RecipeEnhancements
     private static final Logger LOGGER = LogManager.getLogger();
 
     public RecipeEnhancements() {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
         MinecraftForge.EVENT_BUS.register(this);
     }
-
-    private void setup(final FMLCommonSetupEvent event)
-    {
-
-    }
-
-    private void doClientStuff(final FMLClientSetupEvent event)
-    {
-
-    }
-
 }
